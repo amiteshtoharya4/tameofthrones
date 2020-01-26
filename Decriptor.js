5091b2cd5188cc732e42d8ab80fcf04785116fc8
Amitesh.Arya <amitesh.arya@in-amitesh.arya>
2020-01-26 13:10:20 +0530

    Amitesh | Refactoring Decriptor Class

--- Decriptor.js
+++ Decriptor.js
@@ -1,10 +1,18 @@
+const ASCII_OF_a = 97;
+const NUMBER_OF_LETTERS = 26;
+
 module.exports = class Decriptor{
     constructor(sender){
         this.key = sender.length;
     }
 
     decriptLetter(letter){
-        const ascii = letter.charCodeAt(0) - 97;
-        return String.fromCharCode((ascii + 26 - this.key) % 26 + 97);
+        const asciiOfEncryptedLetter = letter.charCodeAt(0);
+        const encryptedLetter = asciiOfEncryptedLetter - ASCII_OF_a;
+        
+        const decryptedLetter = (encryptedLetter + NUMBER_OF_LETTERS - this.key) % NUMBER_OF_LETTERS;
+        const asciiOfDecryptedLetter = decryptedLetter + ASCII_OF_a;
+
+        return String.fromCharCode(asciiOfDecryptedLetter);
     }
 }
