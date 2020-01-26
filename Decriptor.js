9fc67b8cd76ea444094ba22fe23e6bc99dc41c5d
Amitesh Arya <amiteshtoharya4@gmail.com>
2020-01-26 13:24:28 +0530

    Amitesh | Decripting words in Decriptor class

--- Decriptor.js
+++ Decriptor.js
@@ -6,13 +6,17 @@ module.exports = class Decriptor{
         this.key = sender.length;
     }
 
-    decriptLetter(letter){
-        const asciiOfEncryptedLetter = letter.charCodeAt(0);
-        const encryptedLetter = asciiOfEncryptedLetter - ASCII_OF_a;
-        
-        const decryptedLetter = (encryptedLetter + NUMBER_OF_LETTERS - this.key) % NUMBER_OF_LETTERS;
-        const asciiOfDecryptedLetter = decryptedLetter + ASCII_OF_a;
+    decriptWord(word){
+        let decriptedWord = '';
+        for(const letter of word){
+            const asciiOfEncryptedLetter = letter.charCodeAt(0);
+            const encryptedLetter = asciiOfEncryptedLetter - ASCII_OF_a;
 
-        return String.fromCharCode(asciiOfDecryptedLetter);
+            const decryptedLetter = (encryptedLetter + NUMBER_OF_LETTERS - this.key) % NUMBER_OF_LETTERS;
+            const asciiOfDecryptedLetter = decryptedLetter + ASCII_OF_a;
+
+            decriptedWord = decriptedWord + String.fromCharCode(asciiOfDecryptedLetter);
+        }
+        return decriptedWord;
     }
 }
--- Decriptor.test.js
+++ Decriptor.test.js
@@ -5,14 +5,20 @@ describe('Decriptor', () => {
     const decriptor = new Decriptor(SENDER);
 
     it('should be able to convert f to a for mohan', () => {
-        const decriptedLetter = decriptor.decriptLetter('f');
+        const decriptedLetter = decriptor.decriptWord('f');
 
         expect(decriptedLetter).toEqual('a');
     });
 
     it('should be able to convert b to w for mohan', () => {
-        const decriptedLetter = decriptor.decriptLetter('b');
+        const decriptedLetter = decriptor.decriptWord('b');
 
         expect(decriptedLetter).toEqual('w');
+    });
+
+    it('should be able to convert owl to jrg for mohan', () => {
+        const decriptedLetter = decriptor.decriptWord('owl');
+
+        expect(decriptedLetter).toEqual('jrg');
     });
 });
