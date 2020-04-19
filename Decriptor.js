b3b358fc6ffd84c2615f6f21247f4ceef97b0c19
Amitesh Arya <amiteshtoharya4@gmail.com>
2020-04-19 14:43:29 +0530

    Amitesh | Change decryptWord signature to accept decryption key

--- Decriptor.js
+++ Decriptor.js
@@ -2,12 +2,10 @@ const ASCII_OF_a = 97;
 const TOTAL_NUMBER_OF_LETTERS = 26;
 
 module.exports = class Decriptor{
-    constructor(sender){
-        this.decryptionKey = sender.length;
-    }
+    constructor(){}
 
-    decryptLetter(positionOfEncryptedLetter) {
-        return (positionOfEncryptedLetter + TOTAL_NUMBER_OF_LETTERS - this.decryptionKey) % TOTAL_NUMBER_OF_LETTERS;
+    decryptLetter(positionOfEncryptedLetter, decryptionKey) {
+        return (positionOfEncryptedLetter + TOTAL_NUMBER_OF_LETTERS - decryptionKey) % TOTAL_NUMBER_OF_LETTERS;
     }
 
     getPositionOfEncryptedLetterInLetterSequence(encryptedLetter){
@@ -15,22 +13,20 @@ module.exports = class Decriptor{
         return asciiOfEncryptedLetter - ASCII_OF_a;
     }
 
-    getDecryptedLetter(encryptedLetter){
+    getDecryptedLetter(encryptedLetter, decryptionKey){
         const positionOfEncryptedLetter = this.getPositionOfEncryptedLetterInLetterSequence(encryptedLetter);        
-        const positionOfDecryptedLetter = this.decryptLetter(positionOfEncryptedLetter);
+        const positionOfDecryptedLetter = this.decryptLetter(positionOfEncryptedLetter, decryptionKey);
         const asciiOfDecryptedLetter = positionOfDecryptedLetter + ASCII_OF_a;
         return String.fromCharCode(asciiOfDecryptedLetter);
     }
 
-    decriptWord(word){
+    decriptWord(word, decryptionKey){
         let decriptedWord = '';
         for(const letter of word){
-            const decryptedLetter = this.getDecryptedLetter(letter);
+            const decryptedLetter = this.getDecryptedLetter(letter, decryptionKey);
 
             decriptedWord = decriptedWord + decryptedLetter;
         }
         return decriptedWord;
     }
-
-    
 }
--- Decriptor.test.js
+++ Decriptor.test.js
@@ -1,23 +1,23 @@
 const Decriptor = require('./Decriptor');
 
 describe('Decriptor', () => {
-    const SENDER = 'mohan';
-    const decriptor = new Decriptor(SENDER);
+    const DECRYPTION_KEY = 5;
+    const decriptor = new Decriptor();
 
-    it('should be able to convert f to a for mohan', () => {
-        const decriptedLetter = decriptor.decriptWord('f');
+    it('should be able to convert f to a for key 5', () => {
+        const decriptedLetter = decriptor.decriptWord('f', DECRYPTION_KEY);
 
         expect(decriptedLetter).toEqual('a');
     });
 
-    it('should be able to convert b to w for mohan', () => {
-        const decriptedLetter = decriptor.decriptWord('b');
+    it('should be able to convert b to w for key 5', () => {
+        const decriptedLetter = decriptor.decriptWord('b', DECRYPTION_KEY);
 
         expect(decriptedLetter).toEqual('w');
     });
 
-    it('should be able to convert owl to jrg for mohan', () => {
-        const decriptedLetter = decriptor.decriptWord('owl');
+    it('should be able to convert owl to jrg for key 5', () => {
+        const decriptedLetter = decriptor.decriptWord('owl', DECRYPTION_KEY);
 
         expect(decriptedLetter).toEqual('jrg');
     });
